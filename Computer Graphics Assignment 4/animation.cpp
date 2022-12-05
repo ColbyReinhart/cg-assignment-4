fb058be4553e00c215b59872294095b042ac0051
ColbyReinhart <colbyreinhart@gmail.com>
2022-12-05 10:14:21 -0500

    Working on robot rework

--- Computer Graphics Assignment 4/animation.cpp
+++ Computer Graphics Assignment 4/animation.cpp
@@ -7,6 +7,66 @@
 #include "main.h"
 
 //
+// VEC3 OVERLOADS
+//
+
+// Vec3 is a cheap copy, so we'll return it by value in overloads
+
+Vec3 Vec3::operator+(const Vec3& rhs) const
+{
+	Vec3 result;
+	result.x = x + rhs.x;
+	result.y = y + rhs.y;
+	result.z = z + rhs.z;
+	return result;
+}
+
+Vec3 Vec3::operator-(const Vec3& rhs) const
+{
+	Vec3 result;
+	result.x = x - rhs.x;
+	result.y = y - rhs.y;
+	result.z = z - rhs.z;
+	return result;
+}
+
+Vec3 Vec3::operator*(const Vec3& rhs) const
+{
+	Vec3 result;
+	result.x = x * rhs.x;
+	result.y = y * rhs.y;
+	result.z = z * rhs.z;
+	return result;
+}
+
+Vec3 Vec3::operator/(const Vec3& rhs) const
+{
+	Vec3 result;
+	result.x = x / rhs.x;
+	result.y = y / rhs.y;
+	result.z = z / rhs.z;
+	return result;
+}
+
+Vec3 Vec3::operator*(const float scalar) const
+{
+	Vec3 result;
+	result.x = x * scalar;
+	result.y = y * scalar;
+	result.z = z * scalar;
+	return result;
+}
+
+Vec3 Vec3::operator/(const float scalar) const
+{
+	Vec3 result;
+	result.x = x / scalar;
+	result.y = y / scalar;
+	result.z = z / scalar;
+	return result;
+}
+
+//
 // StaticModel
 //
 
@@ -78,5 +138,155 @@ void Tree::draw(GLuint* textureIDs) const
 	glPopMatrix();
 
 	// Pop the used matrix
+	glPopMatrix();
+}
+
+//
+// DynamicModel
+//
+
+DynamicModel::DynamicModel(const struct Vec3& pos, const struct Vec3& rot, const struct Vec3& scale)
+{
+	pos_ = pos;
+	rot_ = rot;
+	scale_ = scale;
+}
+
+//
+// Robot : DynamicModel
+//
+
+Robot::Robot()
+{
+	// Fill the joints map
+	joints_["left shoulder"] = 0;
+	joints_["left elbow"] = 0;
+	joints_["right shoulder"] = 0;
+	joints_["right elbow"] = 0;
+	joints_["left hip"] = 0;
+	joints_["left knee"] = 0;
+	joints_["right hip"] = 0;
+	joints_["right knee"] = 0;
+}
+
+void Robot::draw() const
+{
+	// Init
+	glPushMatrix();
+
+	// Set up rotation and translation
+	glTranslatef(0, 2.5f, 0);	// Move center to point of rotation
+	glRotatef(rot_.x, 1, 0, 0);	// Rotate
+	glRotatef(rot_.y, 0, 1, 0);
+	glRotatef(rot_.z, 0, 0, 1);
+	glTranslatef(pos_.x, pos_.y - 2.5f, pos_.z); // Translate to actual position
+	glColor3f(1, 1, 1); // Color suitable for texturing
+
+	// Draw the head
+	glPushMatrix();
+	glTranslatef(0, 3.65, 0);
+	wireframe_ ? glutWireCube(0.8f) : solidCube(0.8f);
+	glPopMatrix();
+
+	// Draw the body
+	glPushMatrix();
+	glTranslatef(0, 2.5, 0);
+	glScalef(1, 1.5, 0.6f);
+	wireframe_ ? glutWireCube(1) : solidCube(1);
+	glPopMatrix();
+
+	// Draw the left arm
+	glPushMatrix();
+	// Upper arm
+	glTranslatef(0.35, 3.1, 0); // Move center to point of rotation
+	glRotatef(getJointRot("left shoulder"), 1, 0, 0); // Rotate joint
+	glTranslatef(0.35, -0.25, 0); // Move into position
+	// Scale and draw
+	glPushMatrix();
+	glScalef(0.4, 0.85, 0.4);
+	wireframe_ ? glutWireCube(1) : solidCube(1);
+	glPopMatrix();
+	// Lower arm
+	glTranslatef(0, -0.5, 0); // Move center to point of rotation
+	glRotatef(getJointRot("left elbow"), 1, 0, 0); // Rotate joint
+	glTranslatef(0, -0.35, 0); // Move into position
+	glPushMatrix();
+	// Scale and draw
+	glScalef(0.4, 0.85, 0.4);
+	wireframe_ ? glutWireCube(1) : solidCube(1);
+	glPopMatrix();
+	// Pop matrix
+	glPopMatrix();
+
+	// Draw the right arm
+	glPushMatrix();
+	// Upper arm
+	glTranslatef(-0.35, 3.1, 0); // Move center to point of rotation
+	glRotatef(getJointRot("right shoulder"), 1, 0, 0); // Rotate joint
+	glTranslatef(-0.35, -0.25, 0); // Move into position
+	// Scale and draw
+	glPushMatrix();
+	glScalef(0.4, 0.85, 0.4);
+	wireframe_ ? glutWireCube(1) : solidCube(1);
+	glPopMatrix();
+	// Lower arm
+	glTranslatef(0, -0.5, 0); // Move center to point of rotation
+	glRotatef(getJointRot("right elbow"), 1, 0, 0); // Rotate joint
+	glTranslatef(0, -0.35, 0); // Move into position
+	glPushMatrix();
+	// Scale and draw
+	glScalef(0.4, 0.85, 0.4);
+	wireframe_ ? glutWireCube(1) : solidCube(1);
+	glPopMatrix();
+	// Pop matrix
+	glPopMatrix();
+
+	// Draw the left leg
+	glPushMatrix();
+	// Thigh
+	glTranslatef(0.3, 1.75, 0); // Move center to point of rotation
+	glRotatef(getJointRot("left hip"), 1, 0, 0); // Rotate joint
+	glTranslatef(0, -0.4, 0); // Move into position
+	// Scale and draw
+	glPushMatrix();
+	glScalef(0.4, 0.85, 0.4);
+	wireframe_ ? glutWireCube(1) : solidCube(1);
+	glPopMatrix();
+	// Knee
+	glTranslatef(0, -0.5, 0); // Move center to point of rotation
+	glRotatef(getJointRot("left knee"), 1, 0, 0); // Rotate joint
+	glTranslatef(0, -0.35, 0); // Move into position
+	glPushMatrix();
+	// Scale and draw
+	glScalef(0.4, 0.85, 0.4);
+	wireframe_ ? glutWireCube(1) : solidCube(1);
+	glPopMatrix();
+	// Pop matrix
+	glPopMatrix();
+
+	// Draw the right leg
+	glPushMatrix();
+	// Thigh
+	glTranslatef(-0.3, 1.75, 0); // Move center to point of rotation
+	glRotatef(getJointRot("right hip"), 1, 0, 0); // Rotate joint
+	glTranslatef(0, -0.4, 0); // Move into position
+	// Scale and draw
+	glPushMatrix();
+	glScalef(0.4, 0.85, 0.4);
+	wireframe_ ? glutWireCube(1) : solidCube(1);
+	glPopMatrix();
+	// Knee
+	glTranslatef(0, -0.5, 0); // Move center to point of rotation
+	glRotatef(getJointRot("right knee"), 1, 0, 0); // Rotate joint
+	glTranslatef(0, -0.35, 0); // Move into position
+	glPushMatrix();
+	// Scale and draw
+	glScalef(0.4, 0.85, 0.4);
+	wireframe_ ? glutWireCube(1) : solidCube(1);
+	glPopMatrix();
+	// Pop matrix
+	glPopMatrix();
+
+	// Pop initial translation matrix
 	glPopMatrix();
 }
--- Computer Graphics Assignment 4/animation.h
+++ Computer Graphics Assignment 4/animation.h
@@ -6,8 +6,26 @@
 #ifndef ANIMATION_H
 #define ANIMATION_H
 
+#include <unordered_map>
+#include <string>
 #include "main.h"
-#include "robot.h"
+
+// A vector with an x, y, and z component,
+// with some simple arithmetic overloads
+struct Vec3
+{
+	float x, y, z;
+
+	// Vector arithmetic overloads
+	Vec3 operator+(const Vec3& rhs) const;
+	Vec3 operator-(const Vec3& rhs) const;
+	Vec3 operator*(const Vec3& rhs) const;
+	Vec3 operator/(const Vec3& rhs) const;
+
+	// Scaler multiplication overloads
+	Vec3 operator*(const float scalar) const;
+	Vec3 operator/(const float scalar) const;
+};
 
 // Base class for handling static models which can be animated.
 // Pos_ represents the center of the model
@@ -39,6 +57,48 @@ public:
 	Tree(){}
 	Tree(const Vec3& pos): StaticModel(pos) {}
 	void draw(GLuint* textureIDs = nullptr) const override;
+};
+
+// Base class for handling dynamic animated models which have moving joints
+// Pos_ represents the center of the model
+class DynamicModel
+{
+public:
+	// Constructors
+	DynamicModel() {}
+	DynamicModel(const struct Vec3& pos, const struct Vec3& rot = { 0 }, const struct Vec3& scale = { 1, 1, 1 });
+	
+	// Joint rotations
+	void rotateJoint(std::string joint, const int rot) { joints_[joint] = rot; }
+	int getJointRot(std::string joint) const { return joints_.at(joint); }
+
+	// Display
+	virtual void draw() const = 0;
+	void useWireframe(const bool use = true) { wireframe_ = use; }
+
+protected:
+	std::unordered_map<std::string, int> joints_;
+	Vec3 pos_ = { 0 };
+	Vec3 rot_ = { 0 };
+	Vec3 scale_ = { 1, 1, 1 };
+	bool wireframe_ = false;
+};
+
+//
+// DynamicModel implementations
+//
+
+class Robot : public DynamicModel
+{
+	Robot();
+	Robot
+	(
+		const struct Vec3& pos,
+		const struct Vec3& rot = { 0 },
+		const struct Vec3& scale = { 1, 1, 1 }
+	) : Robot() { pos_ = pos; rot_ = rot; scale_ = scale; }
+
+	virtual void draw() const override;
 };
 
 #endif // ANIMATION_H
